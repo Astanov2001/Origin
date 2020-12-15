df03cc3caebad9a26e425819be1c1781491f50ae
Ali <74265866+Astanov2001@users.noreply.github.com>
2020-12-15 17:52:46 +0200

    Test

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
-const arrayString=prompt('Пиши числа через запятую');
-const array=arrayString.split(',').map(Number);
-const reducer=(accumulator, currentValue) => accumulator + currentValue;
 
-const answer=prompt('How old are you');
+let monster1 = {
+    name: "Ali", 
+    age: 19,
+    damage: Math.floor(Math.random()* 15),
+    win: false
+};
 
-console.log(array.reduce(reducer) / array.length);
+let monster2 = {
+    name: 'Oleg',
+    age: 19,
+    damage: Math.floor(Math.random()* 15),
+    win: false
+}
 
-console.log(answer+' Лет');
+console.log("Урон Али = " + monster1.damage);
+console.log("Урон Олега = " + monster2.damage);
+
+let winner;
+
+if (monster1.damage > monster2.damage) {
+    console.log ("Ali Winner")
+    winner = monster1.damage
+    monster1.win = true
+    console.log("Победитель Али Урон: " + winner);
+} else if (monster1.damage < monster2.damage) {
+    console.log ("Oleg Winner")
+    winner = monster2.damage
+    monster2.win = true
+    console.log("Победитель Олег Урон: " + winner);
+} else if (monster1.damage == monster2.damage) {
+    console.log ("Ничья")
+}
+
+let monster3 = {
+    name: "Boss",
+    age: 100,
+    damage: Math.floor(Math.random()* 20)
+}
+
+console.log ("Урон Босса " + monster3.damage)
+
+if (monster3.damage > winner) {
+    console.log("Босс Winner")
+}else if (monster3.damage < winner){
+    console.log("Босс Lose")
+}else if (monster3.damage == winner) {
+    console.log ("Ничья")
+}
